35552a0d0028e781aa8abfaffa8dc4f558694e90
akelstrom <akelstrom@gmail.com>
2020-11-05 16:18:40 -0500

    start edit task feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,24 +98,48 @@ var createTaskActions = function(taskId) {
     return actionContainerEl;
 };
 
-//on a submit click, create a task-- submit event listener responds to entire form, with id of submit
-    formEl.addEventListener("submit", taskFormHandler);
 
 var taskButtonHandler = function(event) {
-    console.log(event.target);
+        // get target element from event
+        var targetEl = event.target;
+      
+        // edit button was clicked
+        if (targetEl.matches(".edit-btn")) {
+          var taskId = targetEl.getAttribute("data-task-id");
+          editTask(taskId);
+        } 
+        // delete button was clicked
+        else if (targetEl.matches(".delete-btn")) {
+          var taskId = targetEl.getAttribute("data-task-id");
+          deleteTask(taskId);
+        }
+      };
 
-    if (event.target.matches(".delete-btn")) {
-        //get the element's task id
-        var taskId = event.target.getAttribute("data-task-id");
-        deleteTask(taskId);
-    }
-};
-
+      
 var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     console.log(taskId);
     taskSelected.remove();
 };
 
-    pageContentEl.addEventListener("click", taskButtonHandler);
+var editTask = function(taskId) {
+    console.log("editing task #" + taskId);
+    // get task list item element
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    // get content from task name and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+
+    formEl.setAttribute("data-task-id", taskId);
+};
+
+//on a submit click, create a task-- submit event listener responds to entire form, with id of submit
+formEl.addEventListener("submit", taskFormHandler);
+    
+pageContentEl.addEventListener("click", taskButtonHandler);
+    
   
