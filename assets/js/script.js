d49ff3a9994bdf062abade9f022ab20db5383574
akelstrom <akelstrom@gmail.com>
2020-11-05 18:09:02 -0500

    added drag and drop feature with some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,8 @@ var createTaskEl = function(taskDataObj) {
 
      //add task id as a custom attribute
      listItemEl.setAttribute("data-task-id", taskIdCounter);
-     
+     listItemEl.setAttribute("draggable", "true");
+
      //create div to hold task info and add to list item
      var taskInfoEl = document.createElement("div");
      //give it a class name
@@ -183,6 +184,54 @@ var taskStatusChangeHandler = function(event) {
       }
 };
 
+var dragTaskHandler = function(event) {
+    var taskId = event.target.getAttribute("data-task-id");
+    event.dataTransfer.setData("text/plain", taskId);
+    var getId = event.dataTransfer.getData("text/plain");
+    console.log("getId:", getId, typeof getId); 
+}
+
+var dropZoneDragHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+      event.preventDefault();
+      taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
+
+      
+    }
+  };
+
+var dropTaskHandler = function(event) {
+    var id = event.dataTransfer.getData("text/plain");
+    var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+    var dropZoneEl = event.target.closest(".task-list");
+    var statusType = dropZoneEl.id;
+    // set status of task based on dropZone id
+    var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+    
+    if (statusType === "tasks-to-do") {
+        statusSelectEl.selectedIndex = 0;
+      } 
+      else if (statusType === "tasks-in-progress") {
+        statusSelectEl.selectedIndex = 1;
+      } 
+      else if (statusType === "tasks-completed") {
+        statusSelectEl.selectedIndex = 2;
+      }
+
+      dropZoneEl.appendChild(draggableElement);
+      dropZoneEl.removeAttribute("style");
+      
+};
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+        if (taskListEl) {
+        taskListEl.removeAttribute("style");
+        }
+  }
+
+
 //on a submit click, create a task-- submit event listener responds to entire form, with id of submit
 formEl.addEventListener("submit", taskFormHandler);
     
@@ -190,3 +239,10 @@ pageContentEl.addEventListener("click", taskButtonHandler);
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
   
+pageContentEl.addEventListener("dragstart", dragTaskHandler);
+
+pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+
+pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
